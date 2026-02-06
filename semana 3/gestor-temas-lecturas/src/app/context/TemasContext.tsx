23b84dff2a3c902233f0bf7de14b510dcc6d82e7
esdras-josue <esdraspea@gmail.com>
2026-02-06 17:01:36 -0600

    fix

---
+++ semana 3/gestor-temas-lecturas/src/app/context/TemasContext.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React, {createContext,useContext, useEffect, useState} from 'react'
+
+export default function TemasContext() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
