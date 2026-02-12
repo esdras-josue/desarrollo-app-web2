d0df1639c498a78b496d6603845871f8b6935084
esdras-josue <esdraspea@gmail.com>
2026-02-11 21:11:00 -0600

    fix

--- semana 4/backend-ecommerce/index.js
+++ semana 4/backend-ecommerce/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const Producto = require('./Modelos/Producto')
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 
 
